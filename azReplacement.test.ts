777248d2f1db018fbe19ce2f4dbd1fcf56038bb3
Hadroberts2512 <hannahaprilroberts@gmail.com>
2022-09-06 19:14:46 -0700

    changed to az

---
+++ azReplacement.test.ts
@@ -0,0 +1,110 @@
+import {eAppPage} from './eAppPageObjects'
+const eApp = new eAppPage()
+
+test('Home Office Login', async () => {
+    await eApp.navigate()
+    await eApp.driver.manage().window().maximize();
+    await eApp.setInput(eApp.userName, 'Homeoffice')
+    await eApp.setInput(eApp.passWord, 'im-kn@ck3r3d')
+    await eApp.click(eApp.signInSubmit)
+
+    await eApp.driver.sleep(10000)
+
+    await eApp.click(eApp.neweApp)
+
+    await eApp.setInput(eApp.state, 'Arizona\n')
+
+    await eApp.click(eApp.create)
+    await eApp.click(eApp.applicantSection)
+
+    await eApp.driver.sleep(10000)
+
+    await eApp.click(eApp.insuredFirstName)
+    await eApp.setInput(eApp.insuredFirstNameXpath, 'ArizonaReplace')
+    await eApp.setInput(eApp.insuredLastName, 'Test')
+    await eApp.setInput(eApp.insuredAddressLine1, '123 Main St')
+    await eApp.setInput(eApp.insuredCity, 'Mesa')
+    
+    await eApp.setInput(eApp.insuredZip, '85203')
+    await eApp.setInput(eApp.insuredPhone, '480-303-8888')
+    await eApp.setInput(eApp.DOB, "09/09/1979\n")
+    await eApp.click(eApp.gender)
+    await eApp.setInput(eApp.gender, "female\n")
+
+//end of applicant info 
+
+    await eApp.click(eApp.healthQuestionsHeader)
+    await eApp.click(eApp.tobaccoNo)
+    await eApp.click(eApp.hivNo)
+    await eApp.click(eApp.hospiceNo)
+    await eApp.click(eApp.heartNo)
+    await eApp.click(eApp.strokeNo)
+    await eApp.click(eApp.cancerNo)
+    await eApp.click(eApp.copdNo)
+    await eApp.click(eApp.organNo)
+    await eApp.click(eApp.dementiaNo)
+    await eApp.click(eApp.drugNo)
+    await eApp.click(eApp.diabetesNo)
+    await eApp.click(eApp.diagnosticTestNo)
+// end of health questions
+
+    await eApp.click(eApp.policySection)
+    await eApp.click(eApp.planTypeDropDown)
+    await eApp.click(eApp.selectFinalExpense)
+
+    eApp.driver.sleep(5000)
+
+    await eApp.setInput(eApp.faceAmount, '20000\n')
+    await eApp.click(eApp.accidentalDeathUnits)
+
+    await eApp.driver.sleep(5000)
+
+    await eApp.click(eApp.noFCGS)
+    await eApp.click(eApp.noAPL)
+    await eApp.click(eApp.mailToOwner)
+
+    await eApp.driver.sleep(4000)
+
+    await eApp.click(eApp.beneSection)
+    await eApp.click(eApp.addBene)
+    await eApp.click(eApp.beneTypeDropDown)
+    await eApp.click(eApp.selectPrimaryBene)
+    await eApp.click(eApp.beneRelationshipDropDown)
+    await eApp.click(eApp.beneRelationshipAunt)
+    await eApp.setInput(eApp.beneFirstNameInput, 'Hannah')
+    await eApp.setInput(eApp.beneLastNameInput, 'Roberts')
+    await eApp.click(eApp.benePercentage)
+    await eApp.click(eApp.beneDone)
+    //end of bene section
+
+    await eApp.click(eApp.payorSection)
+    await eApp.click(eApp.payorSameAsOwner)
+    await eApp.click(eApp.billModeDropDown)
+    await eApp.click(eApp.selectModeMonthly)
+    await eApp.setInput(eApp.initialPayDateInput, '10/01/2022')
+    await eApp.click(eApp.billTypeDropDown)
+    await eApp.click(eApp.selectBankDraft)
+    await eApp.setInput(eApp.recurringDayofMonth, '1')
+    await eApp.setInput(eApp.routing, '122100024')
+    await eApp.setInput(eApp.account, '123456')
+    await eApp.click(eApp.accountTypeDropDown)
+    await eApp.click(eApp.accountTypeChecking)
+    await eApp.setInput(eApp.financialInstitution, 'Chase Bank')
+    await eApp.setInput(eApp.bankCity, 'Phoenix')
+    await eApp.click(eApp.futurePaymentsYes)
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    await eApp.driver.sleep(4000)
+    await eApp.driver.quit
+})
--- eAppPageObjects.ts
+++ eAppPageObjects.ts
@@ -8,7 +8,7 @@ passWord: By = By.id('password')
 //end of login page
 
 
-neweApp: By = By.xpath(('//button[@class="ui blue right floated button new-eapp-button"]'))
+neweApp: By = By.xpath('//button[@class="ui blue right floated button new-eapp-button"]')
 
 state: By = By.xpath(('(//input[@class="search"])[1]'))
 languageSelection: By = By.xpath(('(//input[@class="search"])[2]'))
@@ -48,22 +48,44 @@ tobaccoNo: By = By.xpath('//div[@name="Health_Questions.TobaccoQuestions_0000000
 hivNo: By = By.xpath('//div[@name="Health_Questions.UninsurabilityQuestions_00000000-0000-0000-0000-000000001292"]//label[contains(text(),"No")]')
 hospiceNo: By = By.xpath('//div[@name="Health_Questions.UninsurabilityQuestions_00000000-0000-0000-0000-000000001293"]//label[contains(text(),"No")]')
 heartNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001283"]//label[contains(text(),"No")]')
+strokeNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001284"]//label[contains(text(),"No")]')
+cancerNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001285"]//label[contains(text(),"No")]')
+copdNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001286"]//label[contains(text(),"No")]')
+organNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001287"]//label[contains(text(),"No")]')
+dementiaNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001288"]//label[contains(text(),"No")]')
+drugNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001289"]//label[contains(text(),"No")]')
+diabetesNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001290"]//label[contains(text(),"No")]')
+diagnosticTestNo: By = By.xpath('//div[@name="Health_Questions.HealthQuestions_00000000-0000-0000-0000-000000001291"]//label[contains(text(),"No")]')
+//end of health questions
+
+policySection: By = By.xpath('//div[@class="title"][normalize-space()="Policy"]')
+planTypeInput: By = By.xpath('//div[@name="Policy_Information.ProductOption"]//input[@type="text"]')
+faceAmount: By = By.xpath('//input[@name="Policy_Information.PrimInsFaceAmount"]')
+yesFCGS: By = By.xpath('//div[@class="ui checked radio checkbox"]//label[contains(text(),"Yes")]')
+noFCGS: By = By.xpath('//div[@name="Policy_Information.FCGSMembership"]//label[contains(text(),"No")]')
+planTypeDropDown: By = By.xpath('//div[@name="Policy_Information.ProductOption"]//i[@class="dropdown icon"]')
+selectFinalExpense: By = By.xpath('//span[normalize-space()="Final Expense"]')
+noAPL: By = By.xpath('//div[@name="Policy_Information.IsAPL"]//label[contains(text(),"No")]')
+mailToOwner: By = By.xpath('//label[normalize-space()="Owner"]')
+selectBest: By = By.xpath('/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[2]/form[1]/div[1]/div[1]/div[4]/div[1]/div[9]/div[1]/div[1]/div[3]/div[3]/button[1]')
+accidentalDeathUnits: By = By.xpath('//input[@name="Policy_Information.ADDCovUnits"]')
+//end policy info 
+
+//Bene section
+beneSection: By = By.xpath('//div[contains(text(),"Beneficiary")]')
+addBene: By = By.xpath('//button[normalize-space()="Add/Edit Beneficiary"]')
+beneTypeDropDown: By = By.xpath('//div[@name="Beneficiary_Information.Beneficiaries[0].BeneficiaryType"]//i[@class="dropdown icon"]')
+selectPrimaryBene: By = By.xpath('//span[normalize-space()="Primary Beneficiary"]')
+beneRelationshipDropDown: By = By.xpath('//div[@name="Beneficiary_Information.Beneficiaries[0].PrimBeneRelationship"]//i[@class="dropdown icon"]')
+beneRelationshipAunt: By = By.xpath('//div[@name="Beneficiary_Information.Beneficiaries[0].PrimBeneRelationship"]//div[@role="listbox"]//div[1]')
+beneFirstNameInput: By = By.xpath('//input[@name="Beneficiary_Information.Beneficiaries[0].FirstName"]')
+beneLastNameInput: By = By.xpath('//input[@name="Beneficiary_Information.Beneficiaries[0].LastName"]')
+benePercentage: By = By.xpath('//input[@name="Beneficiary_Information.Beneficiaries[0].Percentage"]')
+beneDone: By = By.xpath('//button[normalize-space()="Done"]')
 
 
 
-
-
-
-planType: By = By.name('Policy_Information.ProductOption')
-face: By = By.name('Policy_Information.PrimInsFaceAmount')
-addRider: By = By.name('Policy_Information.ADDCovUnits')
-childRiderUnits: By = By.name('Policy_Information.ChildRiderCovUnits')
-numberChildRiders: By = By.name('Policy_Information.NumberOfChildren')
-//end of plan info 
-
-addBene: By = By.xpath(('//button[text()="Add/Edit Beneficiary"]'))
-
-
+ownerSection: By = By.name('//div[contains(text(),"Owner")]')
 ownerFirstName: By = By.name('Owner_Information.OwnerFirstName')
 onwerMiddleName: By = By.name('Owner_Information.OwnerMiddleName')
 ownerLastName: By = By.name('Owner_Information.OwnerLastName')
@@ -74,7 +96,8 @@ ownerState: By = By.name('Owner_Information.OwnerState')
 ownerZip: By = By.name('Owner_Information.OwnerZip')
 //end of owner info 
 
-payorSameAsOwner: By = By.name('Payor_Information.PayorOwnerFlag')
+payorSection: By = By.xpath('//div[contains(text(),"Payor")]')
+payorSameAsOwner: By = By.xpath('//div[@name="Payor_Information.PayorOwnerFlag"]//div[@class="ui fitted toggle checkbox"]//label')
 payorFirstName: By = By.name('Payor_Information.PayorFirstName')
 payorMiddleName: By = By.name('Payor_Information.PayorMiddleName')
 payorLastName:  By = By.name('Payor_Information.PayorLastName')
@@ -83,12 +106,20 @@ payorAddress: By = By.name('Payor_Information.PayorLine1')
 payorCity: By = By.name('Payor_Information.PayorCity')
 payorState: By = By.name('Payor_Information.PayorState')
 payorZipCode: By = By.name('Payor_Information.PayorZip')
-billMode: By = By.name('Payor_Information.BillingFrequency')
-initialPayDate: By = By.name('Payor_Information.InitialPaymentDate')
-billType: By = By.name('Payor_Information.PremType')
+billModeDropDown: By = By.xpath('//div[@name="Payor_Information.BillingFrequency"]//i[@class="dropdown icon"]')
+selectModeMonthly: By = By.xpath('//span[normalize-space()="Monthly"]')
+initialPayDateInput: By = By.xpath('//input[@name="Payor_Information.InitialPaymentDate"]')
+billTypeDropDown: By = By.xpath('//div[@name="Payor_Information.PremType"]//i[@class="dropdown icon"]')
+selectBankDraft: By = By.xpath('//div[@class="visible menu transition"]//span[@class="text"][normalize-space()="Bank Draft"]')
 dueDay: By = By.name('Payor_Information.DueDay')
-routing: By = By.name('Payor_Information.RoutingTransitNum')
-account: By = By.name('Payor_Information.BankAccountNum')
+routing: By = By.xpath('//input[@name="Payor_Information.RoutingTransitNum"]')
+account: By = By.xpath('//input[@name="Payor_Information.BankAccountNum"]')
+recurringDayofMonth: By = By.xpath('//input[@name="Payor_Information.DueDay"]')
+accountTypeDropDown: By = By.xpath('//div[@name="Payor_Information.BankAccountType"]//i[@class="dropdown icon"]')
+accountTypeChecking: By = By.xpath('//div[@name="Payor_Information.BankAccountType"]//div[@role="listbox"]//div[1]')
+financialInstitution: By = By.xpath('//input[@name="Payor_Information.FinancialInst"]')
+bankCity: By = By.xpath('//input[@name="Payor_Information.BankCity"]')
+futurePaymentsYes: By = By.xpath('//div[@name="Payor_Information.FuturePaymentQuestion"]//label[contains(text(),"Yes")]')
 //end of payor info
 
 
